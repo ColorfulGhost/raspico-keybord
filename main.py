0f92403b23cfe1412026fe660ef1d61a76676d53
ColorfulGhost <815666528@qq.com>
2022-07-16 22:42:54 +0800

    1-9

--- main.py
+++ main.py
@@ -8,19 +8,38 @@ from lib.adafruit_hid.keycode import Keycode
 
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
+
 keyboard = Keyboard(usb_hid.devices)
 
-layout=KeyboardLayoutUS(keyboard)
-keyboard.send(Keycode.WINDOWS,Keycode.R)
-time.sleep(0.11)
-layout.write("cmd\n")
+KEY_GP_MAP = [
+    [board.GP1, Keycode.ONE],
+    [board.GP2, Keycode.TWO],
+    [board.GP3, Keycode.THREE],
+    [board.GP4, Keycode.FOUR],
+    [board.GP5, Keycode.FIVE],
+    [board.GP6, Keycode.SIX],
+    [board.GP7, Keycode.SEVEN],
+    [board.GP8, Keycode.EIGHT],
+    [board.GP9, Keycode.NINE]
+]
+
+buttons = []
+
+
+for key in KEY_GP_MAP:
+    btn = digitalio.DigitalInOut(key[0])
+    btn.direction = digitalio.Direction.INPUT
+    btn.pull = digitalio.Pull.DOWN
+    buttons.append(btn)
+
 
 while True:
-    time.sleep(1)
-    led.value = True
-    time.sleep(1)
-    # keyboard.press()
-    # keyboard.press(Keycode.ONE)
+    time.sleep(0.1)
+    for index,btn in enumerate(buttons):
+        if btn.value:
+            led.value = True
+            # print(index)
+            # print(KEY_GP_MAP[i][1])
+            keyboard.press(KEY_GP_MAP[index][1])
+            keyboard.release(KEY_GP_MAP[index][1])
     led.value = False
-
-# if __name__ == '__main__':
